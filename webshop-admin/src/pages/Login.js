46df9eb8afa82366e050f75fe1ebe4d1d8938901
hoaihv <hoai.hv@zinza.com.vn>
2021-08-03 16:35:45 +0700

    initial admin app

---
+++ webshop-admin/src/pages/Login.js
@@ -0,0 +1,184 @@
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography
+} from '@material-ui/core';
+import FacebookIcon from 'src/icons/Facebook';
+import GoogleIcon from 'src/icons/Google';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Helmet>
+        <title>Login | Material Kit</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          justifyContent: 'center'
+        }}
+      >
+        <Container maxWidth="sm">
+          <Formik
+            initialValues={{
+              username: 'toidaidot',
+              password: '123456'
+            }}
+            validationSchema={Yup.object().shape({
+              username: Yup.string().min(4).max(255).required('Username is required'),
+              password: Yup.string().max(255).required('Password is required')
+            })}
+            onSubmit={() => {
+              navigate('/app/dashboard', { replace: true });
+            }}
+          >
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Box sx={{ mb: 3 }}>
+                  <Typography
+                    color="textPrimary"
+                    variant="h2"
+                  >
+                    Sign in
+                  </Typography>
+                  <Typography
+                    color="textSecondary"
+                    gutterBottom
+                    variant="body2"
+                  >
+                    Sign in on the internal platform
+                  </Typography>
+                </Box>
+                <Grid
+                  container
+                  spacing={3}
+                >
+                  <Grid
+                    item
+                    xs={12}
+                    md={6}
+                  >
+                    <Button
+                      color="primary"
+                      fullWidth
+                      startIcon={<FacebookIcon />}
+                      onClick={handleSubmit}
+                      size="large"
+                      variant="contained"
+                    >
+                      Login with Facebook
+                    </Button>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    md={6}
+                  >
+                    <Button
+                      fullWidth
+                      startIcon={<GoogleIcon />}
+                      onClick={handleSubmit}
+                      size="large"
+                      variant="contained"
+                    >
+                      Login with Google
+                    </Button>
+                  </Grid>
+                </Grid>
+                <Box
+                  sx={{
+                    pb: 1,
+                    pt: 3
+                  }}
+                >
+                  <Typography
+                    align="center"
+                    color="textSecondary"
+                    variant="body1"
+                  >
+                    or login with email address
+                  </Typography>
+                </Box>
+                <TextField
+                  error={Boolean(touched.username && errors.username)}
+                  fullWidth
+                  helperText={touched.username && errors.username}
+                  label="User name"
+                  margin="normal"
+                  name="username"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="username"
+                  value={values.username}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.password && errors.password)}
+                  fullWidth
+                  helperText={touched.password && errors.password}
+                  label="Password"
+                  margin="normal"
+                  name="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="password"
+                  value={values.password}
+                  variant="outlined"
+                />
+                <Box sx={{ py: 2 }}>
+                  <Button
+                    color="primary"
+                    disabled={isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Sign in now
+                  </Button>
+                </Box>
+                <Typography
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  Don&apos;t have an account?
+                  {' '}
+                  <Link
+                    component={RouterLink}
+                    to="/register"
+                    variant="h6"
+                  >
+                    Sign up
+                  </Link>
+                </Typography>
+              </form>
+            )}
+          </Formik>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
